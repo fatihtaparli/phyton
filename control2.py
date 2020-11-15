34e4127e12b2ed956798149a797a7d6596e6c2a0
fatih taparli <fatiihtaparlii@gmail.com>
2020-11-15 14:45:22 +0100

    Added control func.

--- control2.py
+++ control2.py
@@ -1,5 +1,23 @@
 from tkinter import *
 import datetime
+def control():
+    file=open("usom.txt", "r")
+    content=file.read()
+    file.close()
+    ip=entry1.get()
+    today=datetime.datetime.now()
+    if str (ip) in content:
+        file=open("log.txt","a")
+        slogan=str(ip)+"malware\nDate:"+str(today)+"\n"
+        file.write(slogan)
+        file.close()
+        v.set("IP is malicious")
+    else:
+        file=open("log.txt","a")
+        slogan= slogan=str(ip)+"is not malware\nDate:"+str(today)+"\n"
+        file.write(slogan)
+        file.close()
+          v.set("IP is not malicious")
 myApp=Tk()
 myApp.title("Malicious IP Control App")
 B=Button(myApp,text="Control",command=control)
